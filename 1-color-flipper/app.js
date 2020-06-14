606a608ae3108ec387bba0e91df7e6c1e387121e
Amine <aminemastouri@gmail.com>
2020-06-15 01:09:49 +0200

    first commit , Color flipper project finished

---
+++ 1-color-flipper/app.js
@@ -0,0 +1,9 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener("click", () => {
+    let randomColor = colors[Math.floor(Math.random()*colors.length)];
+    color.textContent = randomColor;
+    document.body.style.backgroundColor = randomColor;
+});
